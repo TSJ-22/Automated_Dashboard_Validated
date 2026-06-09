4d2f847043a2d70347505e415e0f89c6e2c02c5f
TSJ-22 <131388352+TSJ-22@users.noreply.github.com>
2026-06-09 18:08:29 +0530

    Update Data_fetch.py

--- Data_fetch.py
+++ Data_fetch.py
@@ -1,6 +1,6 @@
 import pandas as pdimport pandas as pd():
     # ✅ Simulated ERP (use Excel now)
-    df = pd.read_excel("data/sample_data.xlsx")
+    df = pd.read_csv("data/sample_data.csv")
 
     df["Date"] = pd.to_datetime(df["Date"])
     df = df.sort_values("Date")
